00c08eadbe365359b8312dd9231718780792f3f5
achyasyahputra <achyasyahputra@gmail.com>
2022-10-19 15:49:03 +0700

    fix-redis and routes

---
+++ hitcount/routes.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from hitcount.utils.hitcount import get_hit_count
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    count = get_hit_count()
+    return 'Hello World! I have been seen {} times.\n'.format(count)
+
+# @app.route("/")
+# def hello_world():
+#     return "<p>Hello, World!</p>"
+
+
+
--- hitcount/utils/hitcount.py
+++ hitcount/utils/hitcount.py
@@ -1,10 +1,9 @@
 import time
 
 import redis
-from flask import Flask
 
-app = Flask(__name__)
 cache = redis.Redis(host='redis', port=6379)
+
 
 def get_hit_count():
     retries = 5
@@ -16,9 +15,3 @@ def get_hit_count():
                 raise exc
             retries -= 1
             time.sleep(0.5)
-
-@app.route('/')
-def hello():
-    count = get_hit_count()
-    return 'Hello World! I have been seen {} times.\n'.format(count)
-
